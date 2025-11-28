a801d26115f5c9b645bd30ec8ecaafbdb055ca19
JulioPMartin <julio.martin@aluno.ifsp.edu.br>
2025-11-28 20:53:05 -0300

    Update flasky.py

--- flasky.py
+++ flasky.py
@@ -9,55 +9,79 @@ from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODifications'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class LoginForm(FlaskForm):
-    email = StringField('Usuário ou E-mail', validators=[DataRequired()])
-    password = PasswordField('Informe a sua senha', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+    nome = StringField('Cadastre o novo Aluno:', validators=[DataRequired()])
+    disciplina = SelectField('Disciplina associada:',
+                             choices=[
+                                 ('DSWA5', 'DSWA5'),
+                                 ('GPSA5', 'GPSA5'),
+                                 ('IHCA5', 'IHCA5'),
+                                 ('SODA5', 'SODA5'),
+                                 ('PJIA5', 'PJIA5'),
+                                 ('TCOA5', 'TCOA5')
+                             ])
+    submit = SubmitField('Cadastrar')
 
-class HomeForm(FlaskForm):
-    nome = StringField('Informe o seu nome', validators=[DataRequired()])
-    sobrenome = StringField('Informe o seu sobrenome', validators=[DataRequired()])
-    instituicao = StringField('Informe a sua Instituição de ensino', validators=[DataRequired()])
-    disciplina = SelectField('Informe a sua disciplina',
-                             choices=[('DSWA5', 'DSWA5'),
-                                      ('DWBA4', 'DWBA4'),
-                                      ('Gestão de Projetos', 'Gestão de Projetos')],
-                             validators=[DataRequired()])
-    submit = SubmitField('Enviar')
+class Alunos(db.Model):
+    __tablename__ = 'alunos'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), nullable=False)
+    disciplina_id = db.Column(db.Integer, db.ForeignKey('disciplinas.id'))
 
-@app.route('/', methods=['GET', 'POST'])
+    def __repr__(self):
+        return f'<Aluno {self.nome}>'
+
+class Disciplinas(db.Model):
+    __tablename__ = 'disciplinas'
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(64), unique=True)
+    alunos = db.relationship('Alunos', backref='disciplina', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Disciplina {self.nome}>'
+
+@app.route('/')
 def index():
     current_time = datetime.utcnow()
-    form = HomeForm()
-    ip_address = request.remote_addr
-    host = request.host
-
-    if form.validate_on_submit():
-        session['nome'] = form.nome.data
-        session['sobrenome'] = form.sobrenome.data
-        session['instituicao'] = form.instituicao.data
-        session['disciplina'] = form.disciplina.data
-        return redirect(url_for('index'))
-
     return render_template(
         'index.html',
-        form=form,
-        nome=session.get('nome'),
-        sobrenome=session.get('sobrenome'),
-        instituicao=session.get('instituicao'),
-        disciplina=session.get('disciplina'),
-        ip_address=ip_address,
-        host=host,
         current_time=current_time
     )
 
 @app.route('/alunos', methods=['GET', 'POST'])
 def alunos():
-    return render_template('alunos.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        nome = form.nome.data
+        disc = form.disciplina.data
+
+        disciplina = Disciplinas.query.filter_by(nome=disc).first()
+
+        if disciplina is None:
+            disciplina = Disciplinas(nome=disc)
+            db.session.add(disciplina)
+            db.session.commit()
+
+        aluno = Alunos(nome=nome, disciplina=disciplina)
+        db.session.add(aluno)
+        db.session.commit()
+
+        flash("Aluno cadastrado com sucesso!")
+        return redirect(url_for('alunos'))
+
+    lista = Alunos.query.all()
+
+    return render_template('alunos.html', form=form, lista=lista)
 
 @app.route('/ocorrencias')
 def ocorrencia():
